89a9cb46e15eeab0bcd8fe0d4caff5665ba3879a
Matthew Kelly <50031389+MatthewKelly1@users.noreply.github.com>
2019-06-16 16:22:23 -0400

    Baisc Queue System to satisfy issues 11/13. Need to fix issues with input but I'll work on that in another issue.

--- database.py
+++ database.py
@@ -6,23 +6,56 @@ import pymongo
 class database:
 
     def __init__(self):
-        self.client = pymongo.MongoClient("mongodb://localhost:27017/")
+        #Change this to where ever you are hosting the database
+        connected = False
+        while connected == False:
+            loc = input("Please enter the database loacation or leave empty for localhost: ")
+
+            if loc == "":
+                try:
+                    self.client = pymongo.MongoClient("mongodb://localhost:27017/",serverSelectionTimeoutMS=1500)
+                    self.client.server_info()
+                except pymongo.errors.ServerSelectionTimeoutError as err:
+
+                    print("Connection Failed")
+                else:
+                    connected = True
+            else:
+
+                try:
+                    self.client = pymongo.MongoClient(loc,serverSelectionTimeoutMS=1500)
+                    self.client.server_info()
+                except pymongo.errors.ServerSelectionTimeoutError as err:
+
+                        print("Connection Failed")
+                else:
+                    connected = True
+
 
         self.db = self.client.queuesystem
 
         self.collection = self.db.line
 
 
-    def addToQueue(self,item):
-        client = {"_id": item.userID, "name:": item.name}
+    def addtoqueue(self,item):
+        client = {"_id": item.userID, "name:": item.name, "reason:":item.reason, "position:": item.position, "uniqueID:": item.uniqueID, "serviceData:": item.serviceData }
         x = self.collection.insert_one(client)
 
-    def removeFromQueue(self,item):
-        client = {"_id": item.userID, "name:": item.name}
+    def removefromqueue(self,item):
+        client = {"_id": item.userID, "name:": item.name, "reason:":item.reason, "position:": item.position,"uniqueID:": item.uniqueID,"serviceData:": item.serviceData}
 
         y = {"_id": item.userID}
 
         self.collection.delete_one(y)
+    def updatePositions(self):
+        for x in self.collection.find():
+            print(x["position:"])
+            self.collection.find_one_and_update({"_id": x["_id"]},{'$inc': {"position:": -1}})
+
+    def callUp(self):
+        x = self.collection.find_one_and_delete({"position:" : 1})
+        self.updatePositions()
+        return x
 
     def isEmpty(self):
         return self.collection.count({}) == 0
@@ -30,10 +63,33 @@ class database:
     def updateDisplay(self):
         pass
 
+    def changePosition(self,id,spot):
+        ## CHECK FOR VALID INPUTS
+        if int(spot) > self.findLastPosition() - 1:
+            return "Out of bounds position please try again"
 
+        current = self.collection.find_one({"_id" : int(id)})
+
+        if int(spot) > current["position:"]:
+            for x in self.collection.find():
+                if (int(spot) >= x["position:"] and x["_id"] != int(id)) and (x["position:"] > current["position:"]):
+                    self.collection.update_one({"_id": x["_id"]}, {"$inc": {"position:": -1 }})
+        elif int(spot) < current["position:"]:
+            for x in self.collection.find():
+                if (int(spot) <= x["position:"] and x["_id"] != int(id))and (x["position:"] < current["position:"]):
+                    self.collection.update_one({"_id": x["_id"]}, {"$inc": {"position:": 1}})
+        self.collection.update_one({"_id": int(id)}, {"$set": {"position:": int(spot)}})
 
     def toList(self):
         return self.collection.find()
+
+    def findLastPosition(self):
+        position = 1
+        for x in self.collection.find():
+            if position <= x["position:"]:
+                position = x["position:"] + 1
+        return position
+
 
     def viewDataBase(self):
         cursor = self.collection.find({})
